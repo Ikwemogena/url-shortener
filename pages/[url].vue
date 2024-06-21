fd040c1323e1a62cf7ec1931a78c37238cedd45e
Ikwemogena Abdulai <abdulaimogena@gmail.com>
2024-06-21 15:25:12 +0100

    chore: Update [url].vue to include loading spinner during redirection

--- pages/[url].vue
+++ pages/[url].vue
@@ -1,5 +1,22 @@
 <template>
-  <div></div>
+  <section class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="max-w-md w-full bg-white p-8 rounded-lg shadow-lg text-center">
+      <h2 class="text-2xl font-bold mb-4">Redirecting...</h2>
+      <p class="mb-4">You will be redirected to your destination shortly.</p>
+      <p class="mb-8">
+        If you are not redirected,
+        <a
+          id="redirectLink"
+          :href="data?.long_url || '/'"
+          class="text-indigo-600 hover:text-indigo-800"
+          >click here</a
+        >.
+      </p>
+      <div
+        class="loader border-t-4 border-indigo-500 rounded-full w-16 h-16 mx-auto"
+      ></div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
@@ -23,4 +40,21 @@ if (data) {
 </script>
 
 <style scoped>
+.loader {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-top: 4px solid #4f46e5;
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
